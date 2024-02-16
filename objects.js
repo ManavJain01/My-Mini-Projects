cee39c777bd9883e5549df0e406a79cead609520
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-02-17 01:10:37 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -2,38 +2,38 @@ let i=1;
 
 const webpages = {
   //------------------------------------------------React-JS-----------------------------------------------------------------------------------
-  [`${i++}`]: {
-    link: './my-project/React Js/Simple-nike-page/index.html',
-    p:'Simple React Project',
-    sp:'Simple Nike Page'
-  },
+  // [`${i++}`]: {
+  //   link: './my-project/React Js/Simple-nike-page/index.html',
+  //   p:'Simple React Project',
+  //   sp:'Simple Nike Page'
+  // },
   //------------------------------------------------JavaScript---------------------------------------------------------------------------------
-  [`${i++}`]: {
-    link: './my-project/JavaScript/Tic-Tac-Toe/Tic-Tac_Toe.html',
-    p:'Simple JavaScript Project',
-    sp:'Tic-Tac-Toe'
-  },
-  [`${i++}`]: {
-    link: './my-project/JavaScript/Rock-Paper-Scissors/Rock-Paper-Scissors.html',
-    p:'Simple JavaScript Project',
-    sp:'Rock-Paper-Scissor'
-  },
-  [`${i++}`]: {
-    link: './my-project/JavaScript/Color-Flipper/colorFlipper.html',
-    p:'Simple Javascript Project',
-    sp:'Color Flipper'
-  },
-  [`${i++}`]: {
-    link: './my-project/JavaScript/Counter/counter.html',
-    p:'Simple Javascript Project',
-    sp:'Counter'
-  }, 
+  // [`${i++}`]: {
+  //   link: './my-project/JavaScript/Tic-Tac-Toe/Tic-Tac_Toe.html',
+  //   p:'Simple JavaScript Project',
+  //   sp:'Tic-Tac-Toe'
+  // },
+  // [`${i++}`]: {
+  //   link: './my-project/JavaScript/Rock-Paper-Scissors/Rock-Paper-Scissors.html',
+  //   p:'Simple JavaScript Project',
+  //   sp:'Rock-Paper-Scissor'
+  // },
+  // [`${i++}`]: {
+  //   link: './my-project/JavaScript/Color-Flipper/colorFlipper.html',
+  //   p:'Simple Javascript Project',
+  //   sp:'Color Flipper'
+  // },
+  // [`${i++}`]: {
+  //   link: './my-project/JavaScript/Counter/counter.html',
+  //   p:'Simple Javascript Project',
+  //   sp:'Counter'
+  // }, 
   //------------------------------------------------Tailwind-----------------------------------------------------------------------------------
-  [`${i++}`]: {
-    link: './my-project/CSS Tailwind/bookmark landing page/bookmarkLanding.html',
-    p:'Simple Tailwind Project',
-    sp:'Bookmark Landing Page'
-  }
+  // [`${i++}`]: {
+  //   link: './my-project/CSS Tailwind/bookmark landing page/bookmarkLanding.html',
+  //   p:'Simple Tailwind Project',
+  //   sp:'Bookmark Landing Page'
+  // }
   //------------------------------------------------Vanilla-CSS---------------------------------------------------------------------------------
   // [`${i++}`]: {
   //   link: './my-projects/CSS/Footer-Header/footer-header.html', //'../my-project/CSS/Footer-Header/Footer-Header.html',
