079361b74a9c1910ae8165f1725849ea91109064
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-02-17 01:00:49 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -3,34 +3,34 @@ let i=1;
 const webpages = {
   //------------------------------------------------React-JS-----------------------------------------------------------------------------------
   [`${i++}`]: {
-    link: '../my-project/React Js/Simple-nike-page/index.html',
+    link: './my-project/React Js/Simple-nike-page/index.html',
     p:'Simple React Project',
     sp:'Simple Nike Page'
   },
   //------------------------------------------------JavaScript---------------------------------------------------------------------------------
   [`${i++}`]: {
-    link: '../my-project/JavaScript/Tic-Tac-Toe/Tic-Tac_Toe.html',
+    link: './my-project/JavaScript/Tic-Tac-Toe/Tic-Tac_Toe.html',
     p:'Simple JavaScript Project',
     sp:'Tic-Tac-Toe'
   },
   [`${i++}`]: {
-    link: '../my-project/JavaScript/Rock-Paper-Scissors/Rock-Paper-Scissors.html',
+    link: './my-project/JavaScript/Rock-Paper-Scissors/Rock-Paper-Scissors.html',
     p:'Simple JavaScript Project',
     sp:'Rock-Paper-Scissor'
   },
   [`${i++}`]: {
-    link: '../my-project/JavaScript/Color-Flipper/colorFlipper.html',
+    link: './my-project/JavaScript/Color-Flipper/colorFlipper.html',
     p:'Simple Javascript Project',
     sp:'Color Flipper'
   },
   [`${i++}`]: {
-    link: '../my-project/JavaScript/Counter/counter.html',
+    link: './my-project/JavaScript/Counter/counter.html',
     p:'Simple Javascript Project',
     sp:'Counter'
   }, 
   //------------------------------------------------Tailwind-----------------------------------------------------------------------------------
   [`${i++}`]: {
-    link: '../my-project/CSS Tailwind/bookmark landing page/index.html',
+    link: './my-project/CSS Tailwind/bookmark landing page/bookmarkLanding.html',
     p:'Simple Tailwind Project',
     sp:'Bookmark Landing Page'
   },
