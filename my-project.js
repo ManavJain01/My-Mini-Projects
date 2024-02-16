521275d4819ce2b300ad5367af040618957ea77f
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-02-17 00:50:06 +0530

    javascript file

---
+++ my-project.js
@@ -0,0 +1,66 @@
+import webpages from "./objects.js";
+//--------------------------------------------------------------------------------------------------------------------------------------------
+/*
+function loadScript(src){
+  const script = document.createElement("script");
+  script.src = src;
+  document.body.prepend(script);
+}
+
+loadScript('./object.js')
+console.log(webpagesX);
+*/
+/*
+document.write("<script src='./object.js'></script>")
+console.log(webpages);
+*/
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+const div3 = document.getElementById('div3');
+const btn = document.getElementById('btn1');
+btn.addEventListener("click", addingWebpages)
+
+async function addingWebpages(){
+  for (let i = 1; i < Object.keys(webpages).length+1; i++) { 
+    await waitforme(1000);
+
+    let div = document.createElement('div');
+    let iframe = document.createElement("iframe");
+    let para = document.createElement("p");
+    let span = document.createElement("span");
+    let alink = document.createElement("a");
+
+    iframe.src = webpages[i].link;
+    iframe.width = "350px";
+    iframe.height = "250px";
+    iframe.className = "webpage";
+
+    para.append(webpages[i].p);
+    para.className = "heading2";
+
+    span.append(webpages[i].sp);
+    span.className = "heading2";
+
+    alink.href = webpages[i].link;
+    alink.target = '_blank';
+    alink.classList.add("webpageLinks");
+
+    div.classList.add('div4');
+
+    div.appendChild(iframe);
+    div.appendChild(para);
+    div.appendChild(span);
+
+    div3.appendChild(alink);
+    alink.appendChild(div);
+
+    btn.disabled = true;
+  }
+}
+
+function waitforme(ms){
+  return new Promise( resolve => {
+    setTimeout(()=>{resolve('')} ,ms)
+  })
+}
