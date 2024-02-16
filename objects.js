df221ca8488bde6098ee845ca4999b3886102372
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-02-17 00:49:49 +0530

    objects file

---
+++ objects.js
@@ -0,0 +1,45 @@
+let i=1;
+
+const webpages = {
+  //------------------------------------------------React-JS-----------------------------------------------------------------------------------
+  [`${i++}`]: {
+    link: '../my-project/React Js/Simple-nike-page/index.html',
+    p:'Simple React Project',
+    sp:'Simple Nike Page'
+  },
+  //------------------------------------------------JavaScript---------------------------------------------------------------------------------
+  [`${i++}`]: {
+    link: '../my-project/JavaScript/Tic-Tac-Toe/Tic-Tac_Toe.html',
+    p:'Simple JavaScript Project',
+    sp:'Tic-Tac-Toe'
+  },
+  [`${i++}`]: {
+    link: '../my-project/JavaScript/Rock-Paper-Scissors/Rock-Paper-Scissors.html',
+    p:'Simple JavaScript Project',
+    sp:'Rock-Paper-Scissor'
+  },
+  [`${i++}`]: {
+    link: '../my-project/JavaScript/Color-Flipper/colorFlipper.html',
+    p:'Simple Javascript Project',
+    sp:'Color Flipper'
+  },
+  [`${i++}`]: {
+    link: '../my-project/JavaScript/Counter/counter.html',
+    p:'Simple Javascript Project',
+    sp:'Counter'
+  }, 
+  //------------------------------------------------Tailwind-----------------------------------------------------------------------------------
+  [`${i++}`]: {
+    link: '../my-project/CSS Tailwind/bookmark landing page/index.html',
+    p:'Simple Tailwind Project',
+    sp:'Bookmark Landing Page'
+  },
+  //------------------------------------------------Vanilla-CSS---------------------------------------------------------------------------------
+  [`${i++}`]: {
+    link: '../my-project/CSS/Footer-Header/Footer-Header.html',
+    p:'Simple CSS Project',
+    sp:'Footer & Header'
+  }
+}
+
+export default webpages;
